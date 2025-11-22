b039ae7f56ad412e770d6ec0d8803c7a33dc599b
Ricardo Rockenbach <02512450040@senacrs.edu.br>
2025-11-22 13:13:31 -0300

    Ajuste jest dentro tsconfig.json

--- SRC/Tests/Pessoa.test.ts
+++ SRC/Tests/Pessoa.test.ts
@@ -10,7 +10,9 @@ class PessoaTeste extends Pessoa {
 
 describe("Teste da Classe Pessoa", () => {
   it("deve criar uma instância da classe filha e acessar os getters", () => {
+    
     const pessoa = new PessoaTeste("João", "Rua X", "9999-9999", "123");
+    
     expect(pessoa.nome).toBe("João");
     expect(pessoa.endereço).toBe("Rua X");
     expect(pessoa.telefone).toBe("9999-9999");
@@ -18,12 +20,14 @@ describe("Teste da Classe Pessoa", () => {
   });
 
   it("deve permitir id nulo", () => {
+    
     const pessoa = new PessoaTeste("Maria", "Rua Y", "8888-8888");
 
     expect(pessoa.id).toBeNull();
   });
 
   it("deve obrigar a implementação do método abstrato listarMembro", () => {
+    
     const pessoa = new PessoaTeste("Ana", "Rua Z", "7777-7777");
 
     // verifica se o método existe
